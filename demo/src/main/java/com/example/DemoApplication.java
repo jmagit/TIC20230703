71f6e5b1563d9d482f644321613fb720e7164fe8
Javier <jmartin@grupoloyal.com>
2023-07-04 19:38:03 +0200

    Entidad

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -11,9 +15,22 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo");
+		System.out.println("Aplicacion arrancada");
+		var actor = new Actor(0, " ", "Grillo");
+			dao.save(actor);
+//		var leido = dao.findById(211);
+//		if(leido.isPresent()) {
+//			actor = leido.get();
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);
+//		}
+//		dao.deleteById(211);
+		dao.findAll().forEach(System.out::println);
 	}
 
 }
