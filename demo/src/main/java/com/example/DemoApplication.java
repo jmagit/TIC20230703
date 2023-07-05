1e1c0d7d0ca547871977ffe0a8090e5d7bb9156c
Javier <jmartin@grupoloyal.com>
2023-07-05 18:41:02 +0200

    Repositorios

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import com.example.domains.contracts.repositories.ActorRepository;
 import com.example.domains.entities.Actor;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -19,6 +24,7 @@ public class DemoApplication implements CommandLineRunner {
 	ActorRepository dao;
 	
 	@Override
+	@Transactional
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicacion arrancada");
 //		var actor = new Actor(0, "Pepito", "Grillo");
@@ -30,7 +36,25 @@ public class DemoApplication implements CommandLineRunner {
 //			dao.save(actor);
 //		}
 //		dao.deleteById(211);
-		dao.findAll().forEach(System.out::println);
+//		dao.findAll().forEach(System.out::println);
+//		dao.findTop5ByFirstNameIgnoreCaseStartingWithOrderByLastNameDesc("p").forEach(System.out::println);
+//		dao.findByActorIdBetween(5, 10, Sort.unsorted() /* Sort.by("actorId").descending()*/).forEach(System.out::println);
+//		dao.findByJPQL(5).forEach(System.out::println);
+//		dao.findBySQL(5).forEach(System.out::println);
+//		dao.findAll((root, query, builder) -> builder.lessThan(root.get("actorId"), 5)).forEach(System.out::println);
+//		dao.findAll((root, query, builder) -> builder.like(root.get("lastName"), "PE%")).forEach(System.out::println);
+//		int page = 1, rows = 20;
+//		dao.findAll(PageRequest.of(page, rows, Sort.by("actorId"))).forEach(System.out::println);
+//		var leido = dao.findById(1);
+//		if(leido.isPresent()) {
+//			var actor = leido.get();
+//			System.out.println(actor);
+//			actor.getFilmActors().forEach(item -> System.out.println(item.getFilm()));
+//		}
+		int page = 1, rows = 10;
+		dao.findAll(PageRequest.of(page, rows, Sort.by("actorId"))).forEach(
+				item -> System.out.println(item.getActorId() + ": " + item.getFilmActors().size())
+		);
 	}
 
 }
--- demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -1,9 +1,22 @@
 package com.example.domains.contracts.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.domains.entities.Actor;
 
-public interface ActorRepository extends JpaRepository<Actor, Integer> {
 
+public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
+	List<Actor> findTop5ByFirstNameIgnoreCaseStartingWithOrderByLastNameDesc(String prefijo);
+	List<Actor> findByActorIdBetween(int inicio, int fin, Sort orderBy);
+	
+	@Query(value = "FROM Actor a WHERE a.actorId < ?1")
+	List<Actor> findByJPQL(int id);
+	@Query(value = "SELECT * FROM actor WHERE actor_id < :id", nativeQuery = true)
+	List<Actor> findBySQL(@Param("id") int id);
 }
